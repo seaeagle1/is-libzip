06b851daee7fcae54f79ca28ca1f5385653d201f
Taylor C. Richberger <taywee@gmx.com>
2020-09-03 16:29:15 -0600

    Add some basic tests and make sure they compile

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,42 @@ use axfive_libzip_sys as ffi;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use tempdir::TempDir;
+    use std::string::String;
+    use std::vec::Vec;
+    use std::ffi::{CString, CStr};
+    use std::os::raw::{c_int, c_void};
+    use std::convert::TryInto;
+    use std::io::Read;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn round_trip() {
+        let tempdir = TempDir::new("test").unwrap();
+        let zip_path = CString::new(tempdir.path().join("file.zip").to_str().unwrap()).unwrap();
+        let foo = "Lorem ipsum dolor sit amet";
+        let bar = "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+
+        {
+            let file_source: source::Source<source::File> = (&zip_path as &CStr).try_into().unwrap();
+            let mut archive = archive::Archive::open(file_source, &[archive::OpenFlag::Create, archive::OpenFlag::Exclusive]).unwrap();
+            let foo_source: source::Source<&[u8]> = foo.as_bytes().try_into().unwrap();
+            archive.add(&CString::new("foo").unwrap(), foo_source, file::Encoding::Guess, false).unwrap();
+            let bar_source: source::Source<&[u8]> = bar.as_bytes().try_into().unwrap();
+            archive.add(&CString::new("bar").unwrap(), bar_source, file::Encoding::Guess, false).unwrap();
+            archive.close().unwrap();
+        }
+
+        {
+            let file_source: source::Source<source::File> = (&zip_path as &CStr).try_into().unwrap();
+            let mut archive = archive::Archive::open(file_source, &[archive::OpenFlag::CheckConsistency, archive::OpenFlag::ReadOnly]).unwrap();
+            let mut foo_buf = String::new();
+            archive.open_file(&CString::new("foo").unwrap(), &[], &[]).unwrap().read_to_string(&mut foo_buf).unwrap();
+            assert_eq!(foo_buf, foo);
+            let mut bar_buf = String::new();
+            archive.open_file(&CString::new("bar").unwrap(), &[], &[]).unwrap().read_to_string(&mut bar_buf).unwrap();
+            assert_eq!(bar_buf, bar);
+            archive.close().unwrap();
+        }
     }
 }

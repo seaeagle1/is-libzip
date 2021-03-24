008e6dfd62325a4cbcfc8f65c296b798379234fc
SeaEagle1 <seaeagle1@users.sourceforge.net>
2021-03-24 19:03:34 +0100

    Fix Github Actions build

--- is-libzip-sys/build.rs
+++ is-libzip-sys/build.rs
@@ -1,6 +1,6 @@
 
 fn main () {
-    println!("cargo:rustc-link-lib=zlib");
+    println!("cargo:rustc-link-lib=z");
     println!("cargo:rustc-link-lib=zip");
 
     println!("cargo:rustc-link-search={}", env!("CARGO_MANIFEST_DIR"));
